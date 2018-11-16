782b105ea2896e995a841c59cdf1ef77b449ae9b
FoxGeoff <fox.geoffrey@gmail.com>
2018-11-15 17:12:32 -0800

    xUnit Tests

---
+++ AutomatedTestingExample/NaturalNumberCalculator.cs
@@ -0,0 +1,49 @@
+﻿namespace AutomatedTestingExample
+{
+    public class NaturalNumberCalculator
+    {
+        ///<summary>
+        ///Add function.
+        ///</summary>
+        ///<param name="a">First number.</param>
+        ///<param name="b">Second number.</param>
+        ///<returns>Returns the sum of passed numbers.</returns>
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        ///<summary>
+        ///Subtract function.
+        ///</summary>
+        ///<param name="a">First number.</param>
+        ///<param name="b">Second number.</param>
+        ///<returns>Returns the differnce between passed numbers.</returns>
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        ///<summary>
+        ///Multiply function.
+        ///</summary>
+        ///<param name="a">First number.</param>
+        ///<param name="b">Second number.</param>
+        ///<returns>Returns the result of mulitiplication.</returns>
+        public int Mulitiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        ///<summary>
+        ///Divide function.
+        ///</summary>
+        ///<param name="a">First number.</param>
+        ///<param name="b">Second number.</param>
+        ///<returns>Returns the result of division.</returns>
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ AutomatedTestingExample/NaturalNumberCalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+
+namespace AutomatedTestingExample
+{
+    public class NaturalNumberCalculatorTests
+    {
+        [Fact]
+        public void AddMethod_Numbers_Success()
+        {
+            var calculator = new NaturalNumberCalculator();
+            var result = calculator.Add(1, 2);
+
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void SubtractMethod_Numbers_Success()
+        {
+            var calculator = new NaturalNumberCalculator();
+            var result = calculator.Subtract(1, 2);
+
+            Assert.Equal(-1, result);
+        }
+
+        [Fact]
+        public void MutiplyMethod_Numbers_Success()
+        {
+            var calculator = new NaturalNumberCalculator();
+            var result = calculator.Mulitiply(3, 2);
+
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void DivideMethod_Numbers_Success()
+        {
+            var calculator = new NaturalNumberCalculator();
+            var result = calculator.Divide(4, 2);
+
+            Assert.Equal(2, result);
+        }
+    }
+}
